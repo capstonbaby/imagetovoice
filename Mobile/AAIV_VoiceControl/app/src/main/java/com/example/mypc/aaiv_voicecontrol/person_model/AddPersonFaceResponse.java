b4517a90aec37ff8fe789aa92d1867890f83d93b
ThanhPT <thanhptse@gmail.com>
2017-03-03 14:03:11 +0700

    delete folder Mobile

--- Mobile/AAIV_VoiceControl/app/src/main/java/com/example/mypc/aaiv_voicecontrol/person_model/AddPersonFaceResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.mypc.aaiv_voicecontrol.person_model;
-
-public class AddPersonFaceResponse{
-    String persistedFaceId;
-
-    public String getPersistedFaceId() {
-        return persistedFaceId;
-    }
-}
--- Mobile/readme.txt
+++
@@ -1,1 +0,0 @@
-this folder using for mobile application
